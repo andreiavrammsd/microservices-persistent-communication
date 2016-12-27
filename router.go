ca74030a06ec6072e958127b6d86782e3b76a160
Andrei Avram <avramandrei@ymail.com>
2016-12-28 00:49:12 +0200

    Split up routes from router.

---
+++ router.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"net/http"
+	"github.com/gorilla/mux"
+	"fmt"
+)
+func NewRouter() *mux.Router {
+	router := mux.NewRouter().StrictSlash(true)
+	for _, route := range routes {
+		router.
+			Methods(route.Method).
+			Path(route.Pattern).
+			Name(route.Name).
+			Handler(route.HandlerFunc)
+	}
+
+	router.NotFoundHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		response := NewResponse(w)
+		response.Body.Error = true
+		response.Body.Message = fmt.Sprintf("%s not found.", r.RequestURI)
+		response.Status = http.StatusNotFound
+		response.Write()
+	})
+
+	return router
+}
--- routes.go
+++ routes.go
@@ -2,8 +2,6 @@ package main
 
 import (
 	"net/http"
-	"github.com/gorilla/mux"
-	"fmt"
 )
 
 type Route struct {
@@ -14,27 +12,6 @@ type Route struct {
 }
 
 type Routes []Route
-
-func NewRouter() *mux.Router {
-	router := mux.NewRouter().StrictSlash(true)
-	for _, route := range routes {
-		router.
-			Methods(route.Method).
-			Path(route.Pattern).
-			Name(route.Name).
-			Handler(route.HandlerFunc)
-	}
-
-	router.NotFoundHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		response := NewResponse(w)
-		response.Body.Error = true
-		response.Body.Message = fmt.Sprintf("%s not found.", r.RequestURI)
-		response.Status = http.StatusNotFound
-		response.Write()
-	})
-
-	return router
-}
 
 var routes = Routes{
 	Route{
